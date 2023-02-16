95d12c2aebaa94665c3c69db2a6c33325b9203ca
Julian Köhler <julkoehl@icloud.com>
2023-02-16 18:40:07 +0100

    adjusted the product details loader function so that it awaits the data before it is being rendered, this way the product picture should not pop in late

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,13 +1,20 @@
-import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, defer, LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import ProductDetailPageLayout from "../components/Layout/ProductDetailPageLayout";
 import firebaseConfig from "../firebase";
 import Product from "../types/Product";
 import { getProductData } from "../util/httpRequests";
 
 const ProductDetails = () => {
-  const productData = useLoaderData() as Product[];
+  const { product } = useLoaderData() as { product: Product[] };
 
-  return <ProductDetailPageLayout product={productData[0]} />;
+  return (
+    <Suspense fallback={<p>Loading</p>}>
+      <Await resolve={product}>
+        {loadedProduct => <ProductDetailPageLayout product={loadedProduct[0]} />}
+      </Await>
+    </Suspense>
+  );
 };
 
 export default ProductDetails;
@@ -15,6 +22,12 @@ export default ProductDetails;
 export async function loader({ params }: LoaderFunctionArgs) {
   const productSlug = params.productSlug;
 
+  return defer({
+    product: await loadProductData(productSlug),
+  });
+}
+
+async function loadProductData(productSlug: string | undefined) {
   const query = `?orderBy="slug"&equalTo="${productSlug}"`;
   return await getProductData(firebaseConfig.dbProductsBySlug, query);
 }
